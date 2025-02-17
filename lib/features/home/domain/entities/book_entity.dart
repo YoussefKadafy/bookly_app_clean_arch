fb8dfca53914e020633e5d19682e899f77c8f05a
YoussefKadafy <kadafyyoussef@gmail.com>
2025-02-17 18:55:27 +0200

    create book entities

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,8 @@
+class BookEntity {
+  final String bookName;
+  final String authorName;
+  final num price;
+  final num rating;
+
+  BookEntity(this.bookName, this.authorName, this.price, this.rating);
+}
