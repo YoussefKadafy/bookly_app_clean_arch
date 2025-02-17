a8298dce284f1c98cfe35f2ba5f045df6edf278e
YoussefKadafy <kadafyyoussef@gmail.com>
2025-02-17 20:13:52 +0200

    create use cases && create fetch featured books use case

---
+++ lib/core/utils/use_cases/use_case.dart
@@ -0,0 +1,8 @@
+import 'package:bookly_app_clean_arch/core/utils/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class UseCase<Type, Param> {
+  Future<Either<Failure, Type>> call([Param parameter]);
+}
+
+class NoParam {}
